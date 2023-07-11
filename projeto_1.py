51b4cde4ec953c11d71315d085ffd45f69198a11
Everton da Silva Paiva <evertonpaiva01@gmail.com>
2023-07-11 11:22:10 -0300

    Plotagem do gráfico Aves/fatalidades

--- projeto_1.py
+++ projeto_1.py
@@ -10,3 +10,23 @@ Aircraft Wildlife Strikes, 1990-2015
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+#Carregando o arquivo dataset
+df = pd.read_csv(r'D:/POLI - UPE\2023.1\LPAA\dataset\database.csv')
+
+# Especie das aves
+especie_ave = np.array(df['Species Name'].loc[(df['Fatalities'] > 0)])
+
+#Tratando o nome das Aves
+aves = np.char.replace(especie_ave.astype(str), 'UNKNOWN ', '')
+
+#Ocorrência de Fatalidade
+fatalidade = np.array(df['Fatalities'].loc[(df['Fatalities'] > 0)])
+legenda = list(aves)
+
+plt.figure(figsize=(18, 8))
+plt.barh(aves,fatalidade.astype(int))
+plt.ylabel('Número de Fatalidades')
+plt.title('Fatalidades/Ave')
+plt.legend(legenda)
+plt.show()
