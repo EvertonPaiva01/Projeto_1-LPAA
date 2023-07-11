f59ba7ac3a4b9a96f60316cedf56ed339da450d8
Everton da Silva Paiva <evertonpaiva01@gmail.com>
2023-07-11 11:34:06 -0300

    Subindo as mudanças

--- projeto_1.py
+++ projeto_1.py
@@ -5,3 +5,26 @@ Aircraft Wildlife Strikes, 1990-2015
 @author: EvertonPaiva01
 
 """
+
+#Importando as bibliotecas que serão utilizadas no projeto
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Carregando o arquivo dataset
+df = pd.read_csv(r'D:/POLI - UPE\2023.1\LPAA\dataset\database.csv')
+
+# Especie das aves
+especie_ave = np.array(df['Species Name'].loc[(df['Fatalities'] > 0)])
+
+#Tratando o nome das Aves
+# aves = np.char.replace(especie_ave.astype(str), 'UNKNOWN ', '')
+
+#Ocorrência de Fatalidade
+fatalidade = np.array(df['Fatalities'].loc[(df['Fatalities'] > 0)])
+
+plt.figure(figsize=(17, 8))
+plt.barh(especie_ave,fatalidade.astype(int))
+plt.xlabel('Ocorrência de Fatalidades')
+plt.title('Fatalidades/Ave')
+plt.show()
