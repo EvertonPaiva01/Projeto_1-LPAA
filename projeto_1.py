bbc237eb242d1b1a56acbdd57181973e8f746f76
Everton da Silva Paiva <evertonpaiva01@gmail.com>
2023-07-11 10:00:56 -0300

    Importando as bibliotecas que serão utilizadas no projeto

--- projeto_1.py
+++ projeto_1.py
@@ -5,3 +5,8 @@ Aircraft Wildlife Strikes, 1990-2015
 @author: EvertonPaiva01
 
 """
+
+#Importando as bibliotecas que serão utilizadas no projeto
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
