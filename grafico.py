ef64fb87c993b56f06d49696cbbb2794b4d55b0c
Everton da Silva Paiva <evertonpaiva01@gmail.com>
2023-07-23 19:16:24 -0300

    Grafico de pizza

--- grafico.py
+++ grafico.py
@@ -29,7 +29,7 @@ class Grafico():
         for bar in bars:
             height = bar.get_height()
             ax.text(bar.get_x() + bar.get_width() / 2, height + 2, f'{height}', ha='center', va='bottom', fontsize=8)
-            
+
         # Título e rótulo do eixo x
         ax.set_title(title, fontsize=16, fontweight='bold')
         ax.set_xlabel(xlabel)
@@ -37,6 +37,7 @@ class Grafico():
         # Exibir o gráfico
         plt.show()
 
+#------Gerando gráfico de linha------#
     def plot(x,y,title,xlabel):
             fig, ax = plt.subplots()
             # Plotar o gráfico de linhas
@@ -44,7 +45,7 @@ class Grafico():
 
             # Adicionar os valores acima de cada pico
             for i in range(len(x)):
-                #percorre os anos e plota os valores em cada pico do gráfico
+                #percorre os anos e plota os valores em cada pico do gráfico da lista dos anos
                 if x[i] in [1990,1995,2000,2005,2010,2013,2014,2015]:
                     ax.text(x[i], y[i], f'{y[i]}', ha='center', va='bottom', fontsize=7, fontweight='bold')
 
@@ -57,3 +58,15 @@ class Grafico():
 
             # Exibir o gráfico
             plt.show()      
+    
+    def pie(x,title,label,legenda):
+        nomes = label
+        #cores=['#4F6272', '#B7C3F3', '#DD7596', '#8EB897']
+        cores=['#4F6272', '#B7C3F3', '#DD7596', '#8EB897','#FFB6C1', '#98FB98', '#B0E0E6', '#FFFF99', '#DDA0DD', '#FFDAB9', '#AFEEEE', '#E6E6FA']
+        fig, ax = plt.subplots()
+        plt.rcParams['figure.figsize'] = (11,7)
+        ax.pie(x,wedgeprops={"linewidth": 1, "edgecolor": "black"}, autopct='%1.1f%%',textprops={'color': 'black'},colors=cores,labels=nomes)
+        ax.set_title(title, fontsize=16, fontweight='bold')
+        if legenda != '':
+            plt.legend(legenda)
+        plt.show()
--- projeto_1.py
+++ projeto_1.py
@@ -29,7 +29,7 @@ fatalidade = np.array(df['Fatalities'].loc[(df['Fatalities'] > 0) & (df['Species
 #Ocorrência de acidentes ao longo dos Anos:
 ano = df['Incident Year'].value_counts().sort_index()
 
-Grafico.plot(ano.index,ano.values,'Evolução dos aceidentes ao longo dos Anos','Ano')
+#Grafico.plot(ano.index,ano.values,'Evolução dos aceidentes ao longo dos Anos','Ano')
 
 
 #-----------------------------------Locais onde ocorre mais colisões----------------------------------------------------------------------#
@@ -42,3 +42,31 @@ for col in df.columns:
         colisoes[col_sep[0]] = df[col_sep[0] + ' Damage'].sum() / df[col].sum()                        
 
 #Grafico.barra(list(colisoes.keys()),list(colisoes.values()),'Alta ocorrência de colisão (%)','')
+
+#----------------------------------------------Análise QUALITATIVA ----------------------------------------------------------------------#
+# Verificando a especie que colidiu mais vezes com aeronaves 
+especies = df["Species Name"]
+qtd_colisao_especies=especies.value_counts()
+
+
+#---------------------Removendo dados com baixa frequencia foram retirados da análise (cerca de 2%)-------------------------------------#
+
+#Selecionando dados com ocorrência acima de 2.000 (duas mil) colisões:
+qtd_colisao_especies=qtd_colisao_especies[qtd_colisao_especies>4000]
+
+#Total de Colisões
+total_colisoes = df['Species Name'].count()
+
+#Baixa ocorrência
+baixa_ocorrencia = total_colisoes - qtd_colisao_especies.sum()
+
+qualitativa = list(qtd_colisao_especies.values)
+qualitativa.append(baixa_ocorrencia)
+
+label = list(qtd_colisao_especies.keys())
+label.append('Baixa Ocorrência')
+
+Grafico.pie(qualitativa,'Especies envolvidas com maior frequência\n em colisões com aeronaves',label,'')
+
+
+#https://www.kaggle.com/code/ronymayukh/vital-data-visualisation
